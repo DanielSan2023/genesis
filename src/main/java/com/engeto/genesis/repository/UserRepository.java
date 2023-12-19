7cb018e0c4e69adf0213a5d2dc3dcf6e732396b4
DanielSan2023 <electronic.san@gmail.com>
2023-12-19 17:50:19 +0100

    added method for work with DB in UserRepository

--- src/main/java/com/engeto/genesis/repository/UserRepository.java
+++ src/main/java/com/engeto/genesis/repository/UserRepository.java
@@ -1,21 +1,69 @@
 package com.engeto.genesis.repository;
 
+import com.engeto.genesis.mapper.UserRowMapper;
 import com.engeto.genesis.model.User;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+
 
 @Component
 public class UserRepository {
+
+    private final UserRowMapper userRowMapper;
     private final JdbcTemplate jdbcTemplate;
 
-    public UserRepository(JdbcTemplate jdbcTemplate) {
+    public UserRepository(JdbcTemplate jdbcTemplate, UserRowMapper userRowMapper) {
         this.jdbcTemplate = jdbcTemplate;
+        this.userRowMapper = userRowMapper;
     }
 
-    public List<User> getAll() {
-        final String sql ="select * from user";
-        return jdbcTemplate.query(sql,userRowMapper);
+    public void createUser(User user) {
+        String sql = "INSERT INTO user (name, surname, person_id, uuid) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getSurname());
+            ps.setString(3, user.getPersonId());
+            ps.setString(4, user.getUuid());
+            return ps;
+        });
     }
+
+    public User getUserById(Long id) {
+        String sql = "select * from user where user.id = " + id;
+        try {
+            return jdbcTemplate.queryForObject(sql, userRowMapper);
+        } catch (EmptyResultDataAccessException e) {
+        }
+        return null;
+
+    }
+
+    public List<User> getAllUsers() {
+        final String sql = "select * from user";
+        return jdbcTemplate.query(sql, userRowMapper);
+    }
+
+    public boolean existsByPersonID(String personID) {
+        String sql = "SELECT COUNT(*) FROM user WHERE person_id = ?";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, personID);
+        return count != null && count > 0;
+    }
+
+
+
 }
+
+
+//    public void deleteStudent(Long id) {
+//        students.removeIf(student -> student.getId().equals(id));
+//    }
+
--- src/main/java/com/engeto/genesis/service/UserService.java
+++ src/main/java/com/engeto/genesis/service/UserService.java
@@ -5,6 +5,8 @@ import com.engeto.genesis.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -14,6 +16,40 @@ public class UserService {
     }
 
     public List<User> getUsers() {
-        return userRepository.getAll();
+        return userRepository.getAllUsers();
     }
+
+    public User get(Long id){
+        return userRepository.getUserById(id);
+    }
+
+    public void createUser(User user){
+
+
+        // Validace personID a ověření, zda uživatel již neexistuje v databázi
+        if (!isValidPersonID(user.getPersonId()) || userRepository.existsByPersonID(user.getPersonId())) {
+            throw new IllegalArgumentException("Neplatné nebo duplicitní personID.");
+        }
+
+        // Vygenerování UUID
+        String uuid = UUID.randomUUID().toString();
+
+        // Vytvoření instance User
+        User newUser = new User();
+        newUser.setName(user.getName());
+        newUser.setSurname(user.getSurname());
+        newUser.setPersonId(user.getPersonId());
+        newUser.setUuid(uuid);
+
+        // Uložení uživatele do databáze
+        userRepository.createUser(newUser);
+    }
+
+    private boolean isValidPersonID(String personID) {
+        // Zde provedete ověření personID podle vašich požadavků
+        // Například můžete ověřit délku, formát, apod.
+        return personID != null && personID.length() == 12;
+    }
+
+
 }
